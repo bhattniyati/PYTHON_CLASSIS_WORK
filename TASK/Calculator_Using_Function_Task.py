7a626944ffafebffdcb490c3ef4ec6f46720728f
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-11 16:06:44 +0530

    Add files via upload

---
+++ TASK/Calculator_Using_Function_Task.py
@@ -0,0 +1,22 @@
+
+
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multi(x,y):
+    return x*y
+
+def divi(x,y):
+    return x/y
+
+
+x=int(input("Enter First Number: "))
+y=int(input("Enter First Number: "))
+print(add(x,y))
+print(sub(x,y))
+print(multi(x,y))
+print(divi(x,y))
